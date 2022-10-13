2ac60bce04f7802a9de3c1662c2806279635e198
emmagine79 <emmagine79@gmail.com>
2022-10-13 14:35:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW']
-  appearance: 'dark', // ['light', 'dark', 'auto'],
+  appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#FFFFFF', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#222222', // use hex value, don't forget '#'
